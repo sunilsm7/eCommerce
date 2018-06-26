1ec9f7bfab28cda656acb7a046bf876618f90c20
Sunil <sunil9766@gmail.com>
2018-06-26 12:51:12 +0530

    add accounts auth app

--- eCommerce/settings/base.py
+++ eCommerce/settings/base.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'tags',
     'carts',
     'orders',
+    'accounts',
 ]
 
 MIDDLEWARE = [
--- eCommerce/urls.py
+++ eCommerce/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-from .views import home_page, about_page, contact_page, login_page, register_page
+from accounts.views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
     url(r'^$', home_page, name='home'),
