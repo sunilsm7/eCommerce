03d547f2965392c2a7d5afc187a9938eb4323b55
Sunil <sunil9766@gmail.com>
2018-06-26 12:05:09 +0530

    Orders app

---
+++ orders/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.db.models.signals import pre_save
+from carts.models import Cart
+from eCommerce.utils import unique_order_id_generator
+# Create your models here.
+
+ORDER_STATUS_CHOICES = (
+    ('created', 'Created'),
+    ('paid', 'Paid'),
+    ('shipped', 'Shipped'),
+    ('refund', 'Refund'),
+)
+
+
+class Order(models.Model):
+    order_id = models.CharField(max_length=120, blank=True)
+    #  billing_profile = ?
+    #  shipping_address
+    #  billing_address
+    cart = models.ForeignKey(Cart, on_delete=None)
+    status = models.CharField(max_length=120, default='created', choices=ORDER_STATUS_CHOICES)
+    shipping_total = models.DecimalField(default=5.99, max_digits=100, decimal_places=2)
+    total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+
+    def __str__(self):
+        return self.order_id
+
+
+#  generate the order id
+
+def pre_save_create_order_id(sender, instance, *args, **kwargs):
+    if not instance.order_id:
+        instance.order_id = unique_order_id_generator(instance)
+
+
+pre_save.connect(pre_save_create_order_id, sender=Order)
+
+#  generate the order total
