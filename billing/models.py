71b24995ef3b4f8bf0731fbbea7bda60a85e0aef
Sunil <sunil9766@gmail.com>
2018-06-27 15:53:05 +0530

    Billing Profile Model

--- billing/models.py
+++ billing/models.py
@@ -1,3 +1,33 @@
+from django.conf import settings
 from django.db import models
-
+from django.db.models.signals import post_save
 # Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+
+class BillingProfile(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True)
+    email = models.EmailField()
+    active = models.BooleanField(default=True)
+    update = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    # customer_id in Stripe or Braintree
+
+    def __str__(self):
+        return self.email
+
+
+# def billing_profile_created_receiver(sender, instance, created, *args, **kwargs):
+#     if created:
+#         instance.customer_id = newID
+#         print("ACTUAL API REQUEST Send to stripe/braintree")
+#         instance.save()
+
+
+def user_created_receiver(sender, instance, created, *args, **kwargs):
+    if created and instance.email:
+        BillingProfile.objects.get_or_create(user=instance, email=instance.email)
+
+
+post_save.connect(user_created_receiver, sender=User)
