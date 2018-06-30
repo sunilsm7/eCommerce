f569d5800ccb7dbaa45a9b3d0004fbe761e1f5d4
Sunil <sunil9766@gmail.com>
2018-06-30 15:38:12 +0530

    Finalize Checkout

--- carts/views.py
+++ carts/views.py
@@ -55,10 +55,19 @@ def checkout_home(request):
             order_obj.shipping_address = Address.objects.get(id=shipping_address_id)
             del request.session["shipping_address_id"]
         if billing_address_id:
-            order_obj.billing_address = Address.objects.get(id=billing_address_id) 
+            order_obj.billing_address = Address.objects.get(id=billing_address_id)
             del request.session["billing_address_id"]
         if billing_address_id or shipping_address_id:
             order_obj.save()
+
+    if request.method == 'POST':
+        """some check that order is done"""
+        is_done = order_obj.check_done()
+        if is_done:
+            order_obj.mark_paid()
+            request.session['cart_items'] = 0
+            del request.session['cart_id']
+            return redirect("/cart/success")
 
     context = {
         "object": order_obj,
