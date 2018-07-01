6808161f9329b64593091e0974e5d5f6b383e877
Sunil <sunil9766@gmail.com>
2018-07-01 16:29:03 +0530

    Update Login & Register Forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,20 +7,20 @@ from .models import User
 User = get_user_model()
 
 
-class RegisterForm(forms.ModelForm):
+class LoginForm(forms.Form):
+    username = forms.EmailField(label='Email')
     password = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+
+
+class RegisterForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ('email',)
-
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        qs = User.objects.filter(email=email)
-        if qs.exists():
-            raise forms.ValidationError("email is taken")
-        return email
+        fields = ('full_name', 'email',)
 
     def clean_password2(self):
         # Check that the two password entries match
@@ -29,6 +29,15 @@ class RegisterForm(forms.ModelForm):
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError("Passwords don't match")
         return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(RegisterForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        user.active = False  # send confirmation email
+        if commit:
+            user.save()
+        return user
 
 
 class UserAdminCreationForm(forms.ModelForm):
@@ -78,37 +87,3 @@ class UserAdminChangeForm(forms.ModelForm):
 
 class GuestForm(forms.Form):
     email = forms.EmailField()
-
-
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
-
-
-class RegisterForm(forms.Form):
-    username = forms.CharField()
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
-
-    def clean_username(self):
-        username = self.cleaned_data.get('username')
-        qs = User.objects.filter(username=username)
-        if qs.exists():
-            raise forms.ValidationError("Username is taken")
-        return username
-
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        qs = User.objects.filter(email=email)
-        if qs.exists():
-            raise forms.ValidationError("email is taken")
-        return email
-
-    def clean(self):
-        data = self.cleaned_data
-        password = self.cleaned_data.get('password')
-        password2 = self.cleaned_data.get('password2')
-        if password2 != password:
-            raise forms.ValidationError("Passwords must match.")
-        return data
