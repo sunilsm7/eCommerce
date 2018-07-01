601704a49ddcb1051fc167103dd103249d593a54
Sunil <sunil9766@gmail.com>
2018-07-01 16:44:35 +0530

    Login and Register Views

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import authenticate, login, get_user_model
-from django.shortcuts import render, redirect
+from django.views.generic import CreateView, FormView
+from django.shortcuts import redirect
 from django.utils.http import is_safe_url
 from .forms import LoginForm, RegisterForm, GuestForm
 from .models import GuestEmail
+
+User = get_user_model()
 
 
 def guest_register_view(request):
@@ -25,19 +28,19 @@ def guest_register_view(request):
     return redirect("/register/")
 
 
-def login_page(request):
-    form = LoginForm(request.POST or None)
-    context = {
-        "form": form
-    }
-    next_ = request.GET.get('next')
-    next_post = request.POST.get('next')
-    redirect_path = next_ or next_post or None
-    if form.is_valid():
-        print(form.cleaned_data)
-        username = form.cleaned_data.get("username")
-        password = form.cleaned_data.get("password")
-        user = authenticate(request, username=username, password=password)
+class LoginView(FormView):
+    form_class = LoginForm
+    success_url = '/'
+    template_name = 'accounts/login.html'
+
+    def form_valid(self, form):
+        request = self.request
+        next_ = request.GET.get('next')
+        next_post = request.POST.get('next')
+        redirect_path = next_ or next_post or None
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request, username=email, password=password)
         if user is not None:
             login(request, user)
             try:
@@ -49,22 +52,10 @@ def login_page(request):
                 return redirect(redirect_path)
             else:
                 return redirect("/")
-        else:
-            # Return an 'invalid login' error message.
-            print("Error")
-
-    return render(request, "accounts/login.html", context)
+        return super(LoginView, self).form_valid(form)
 
 
-User = get_user_model()
-
-
-def register_page(request):
-    form = RegisterForm(request.POST or None)
-    context = {
-        "form": form
-    }
-    if form.is_valid():
-        form.save()
-
-    return render(request, "accounts/register.html", context)
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = 'accounts/register.html'
+    success_url = '/login/'
