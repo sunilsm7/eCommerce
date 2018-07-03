1ec85f7b07135c0f3c9c349a7e4f216b6183d3b5
Sunil <sunil9766@gmail.com>
2018-07-03 15:42:53 +0530

    Create Stripe Customer

--- billing/models.py
+++ billing/models.py
@@ -1,10 +1,16 @@
+import logging
+import stripe
 from django.conf import settings
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from accounts.models import GuestEmail
 # Create your models here.
 
+logger = logging.getLogger(__name__)
+
 User = settings.AUTH_USER_MODEL
+
+stripe.api_key = "sk_test_0bfLJF5yaMsFBrcGsCYsVWnP"
 
 
 class BillingProfileManager(models.Manager):
@@ -31,6 +37,7 @@ class BillingProfile(models.Model):
     active = models.BooleanField(default=True)
     update = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+    customer_id = models.CharField(max_length=120, null=True, blank=True)
     # customer_id in Stripe or Braintree
 
     objects = BillingProfileManager()
@@ -39,15 +46,23 @@ class BillingProfile(models.Model):
         return self.email
 
 
-# def billing_profile_created_receiver(sender, instance, created, *args, **kwargs):
-#     if created:
-#         instance.customer_id = newID
-#         print("ACTUAL API REQUEST Send to stripe/braintree")
-#         instance.save()
+def billing_profile_created_receiver(sender, instance, *args, **kwargs):
+    if not instance.customer_id and instance.email:
+        logger.warning("ACTUAL API REQUEST Send to stripe")
+        customer = stripe.Customer.create(
+            email=instance.email
+        )
+        logger.warning('customer {}'.format(customer))
+        instance.customer_id = customer.id
+
+
+pre_save.connect(billing_profile_created_receiver, sender=BillingProfile)
 
 
 def user_created_receiver(sender, instance, created, *args, **kwargs):
+    logger.warning('in billing_profile_created_receiver')
     if created and instance.email:
+        logger.warning('creating user profile')
         BillingProfile.objects.get_or_create(user=instance, email=instance.email)
 
 
