0aa7ea5d5c7b1e5dbbb5bde759a0a3963bc5db12
Sunil <sunil9766@gmail.com>
2018-07-04 14:55:41 +0530

    Changing Payment Methods

--- billing/models.py
+++ billing/models.py
@@ -1,6 +1,7 @@
 import logging
 import stripe
 from django.conf import settings
+from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import post_save, pre_save
 from accounts.models import GuestEmail
@@ -51,6 +52,9 @@ class BillingProfile(models.Model):
     def get_cards(self):
         return self.card_set.all()
 
+    def get_payment_method_url(self):
+        return reverse('billing-payment-method')
+
     @property
     def has_card(self):
         # instance has card
@@ -59,8 +63,8 @@ class BillingProfile(models.Model):
 
     @property
     def default_card(self):
-        default_cards = self.get_cards().filter(default=True)
-        if default_cards.exist():
+        default_cards = self.get_cards().filter(active=True, default=True)
+        if default_cards.exists():
             return default_cards.first()
         return None
 
@@ -132,6 +136,16 @@ class Card(models.Model):
 
     def __str__(self):
         return "{} {}".format(self.brand, self.last4)
+
+
+def new_card_post_save_receiver(sender, instance, created, *args, **kwargs):
+    if instance.default:
+        billing_profile = instance.billing_profile
+        qs = Card.objects.filter(billing_profile=billing_profile).exclude(pk=instance.pk)
+        qs.update(default=False)
+
+
+post_save.connect(new_card_post_save_receiver, sender=Card)
 
 
 class ChargeManager(models.Manager):
