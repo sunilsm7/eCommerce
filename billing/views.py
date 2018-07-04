0a0ada008c000729fd18b6e844200803291812b2
Sunil <sunil9766@gmail.com>
2018-07-04 12:56:56 +0530

    Charge the Customer

--- billing/views.py
+++ billing/views.py
@@ -42,9 +42,7 @@ def payment_method_createview(request):
 
         token = request.POST.get("token")
         if token is not None:
-            customer = stripe.Customer.retrieve(billing_profile.customer_id)
-            card_response = customer.sources.create(source=token)
-            new_card_obj = Card.objects.add_new(billing_profile, card_response)
+            new_card_obj = Card.objects.add_new(billing_profile, token)
             logger.warning(new_card_obj)
         return JsonResponse({'message': "Success! Your card was added."})
     return HttpResponse("error", status=401)
